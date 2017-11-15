aaade2ee925e8131154859c08a7a23c7053bd582
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2017-11-15 12:20:14 +0100

    eliminado la clase

--- ej_javascript27/js/main.js
+++ ej_javascript27/js/main.js
@@ -5,7 +5,7 @@ window.onload = function(){
     
     var escribirResolucion = document.getElementsByTagName('p')[0];
     escribirResolucion.textContent ="Tu resolución es "+resolucionPantalla;
-    var errorResoluc = document.getElementsByClassName('errorResolucion');
+   
     for(i=0;i<longitud;i++){
         if(cogerInput[i].type=="radio" && cogerInput[i].id==resolucionPantalla){
             cogerInput[i].checked = true;
