2453df569804aa3f96668955822d80e2aab0e86e
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2017-11-15 19:28:01 +0100

    generar palabraAleatoria

--- ej_javascript31/js/main.js
+++ ej_javascript31/js/main.js
@@ -1,4 +1,23 @@
+
+
 window.onload = function(){
    
+
+    var arrayPalabras = ["javascript", "ajax", "html", "css", "programación"];
+
+    var palabraGenerada = generarPalabaAleatoria(arrayPalabras);
+}
+
+
+
+//funcion que le pasas un arrary de palaras y te dvuelve una palabraaleatoria
+function generarPalabaAleatoria(arrayDePalabras){
+
+   
+
+   var palabraAleatoria = Math.floor(Math.random()*arrayDePalabras.length);
+
+   
+   return arrayDePalabras[palabraAleatoria];
 }
 
