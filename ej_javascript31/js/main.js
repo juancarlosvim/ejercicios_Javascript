29d4bfd4f98edc506ab3e8d6a1bd3556bfbad83d
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2017-11-16 12:17:07 +0100

    añadida la imagen al cargar, falta la funcion para comprobar las imágenes

--- ej_javascript31/js/main.js
+++ ej_javascript31/js/main.js
@@ -1,4 +1,5 @@
 
+vidas =7;
 
 window.onload = function(){
    
@@ -6,6 +7,9 @@ window.onload = function(){
     var arrayPalabras = ["javascript", "ajax", "html", "css", "programación"];
 
     var palabraGenerada = generarPalabaAleatoria(arrayPalabras);
+    
+    colocarImagen();
+
 }
 
 
@@ -21,3 +25,23 @@ function generarPalabaAleatoria(arrayDePalabras){
    return arrayDePalabras[palabraAleatoria];
 }
 
+//funcion comprobar las fotos
+/* <img src = onError o onLoad */
+function comprobarFotos(numero){
+
+   for(i=0; i<numero.length;i++){
+
+
+   }
+
+}
+// muestro la presentacion del juego
+function colocarImagen(){
+    var cogerImagen = document.getElementsByTagName('img')[0];
+    
+    cogerImagen.src="img/0.png";
+    
+    var ponerVidas = document.getElementById('vidas');
+    ponerVidas.textContent="Número de vidas: "+vidas; 
+    
+}
