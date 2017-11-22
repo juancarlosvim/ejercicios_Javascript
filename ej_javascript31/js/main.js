9de37ad3a9bd95a564996744fa949a723b444d13
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2017-11-22 12:14:42 +0100

    actualizado ejercicio 31

--- ej_javascript31/js/main.js
+++ ej_javascript31/js/main.js
@@ -5,14 +5,18 @@ var ruta = "img/" //ruta generica
 var debug = true // para imprimir en consola
 window.onload = function(){
 
-    var arrayPalabras = ["javascript", "ajax", "html", "css", "programación"];
+    var arrayPalabras = ["javascript", "ajax", "html", "css", "programacion"];
 
     var arrayImagenes = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png"];
 
     var palabraGenerada = generarPalabaAleatoria(arrayPalabras);
 
+
     rellenarPalabra(palabraGenerada);
     colocarImagen();
+    accionBotones();
+
+
 
 
 }
@@ -23,7 +27,28 @@ window.onload = function(){
 function generarPalabaAleatoria(arrayDePalabras){
 
    var palabraAleatoria = Math.floor(Math.random()*arrayDePalabras.length);
-   return arrayDePalabras[palabraAleatoria];
+   return arrayDePalabras[palabraAleatoria].toUpperCase();
+}
+/* funcion cogerBotones */
+function  accionBotones() {
+    var cogerBotones = document.getElementsByTagName('button');
+    var longitudBotones = cogerBotones.length;
+    for (i = 0; i < longitudBotones; i++) {
+        cogerBotones[i].addEventListener("click", botonesValor);
+
+    }
+
+}
+/* funcion que da el valor cuando haces click en el boton */
+function botonesValor(e) {
+    if(debug){
+        //console.log(e.target.value);
+    }
+    var valorLetra = e.target.value;
+    if(debug){
+        console.log(valorLetra);
+    }
+    return valorLetra;
 }
 
 /* funcion relllenar palabra _ _ _ */
